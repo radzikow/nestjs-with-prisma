c895890f901be66aaf0a5e248a1dc35caeab9844
Radosław Bura <radoslaw.bura@netguru.com>
2022-11-28 17:55:52 +0100

    Add validation pipes

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,21 @@
+import { INestApplication, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  loadGlobalPipes(app);
+  loadSwagger(app);
+  await app.listen(3000);
+}
+bootstrap();
 
-  // Swagger config
+const loadGlobalPipes = (app: INestApplication) => {
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+};
+
+const loadSwagger = (app: INestApplication) => {
   const config = new DocumentBuilder()
     .setTitle('Median')
     .setDescription('The Median API description')
@@ -14,8 +24,4 @@ async function bootstrap() {
 
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
-
-  await app.listen(3000);
-}
-
-bootstrap();
+};
